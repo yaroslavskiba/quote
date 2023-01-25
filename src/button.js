b2fd4b125c7d0a26492fb6e4221219dbaeb269d9
yaroslav skiba <109871666+yaroslavskiba@users.noreply.github.com>
2023-01-25 21:32:47 +0300

    context

--- src/button.js
+++ src/button.js
@@ -1,8 +1,16 @@
-import React, { useState, useEffect, useContext, Fragment } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { FaTumblr, FaTwitter, FaCarrot } from "react-icons/fa";
+import { MyContext } from './index';
 
-const backgrounds = ['#aee2f6', '#002f49', '#c18f6e', '#72c2f4', '#009ccf', '#601f2d']
-const quotes = [{phrase: 'For freecodecamp', author: 'yaroslav'}, {phrase: 'in vino veritas', author: 'skiba'}];
+const backgrounds = ['#E78F63', '#AD4A2B', '#A7BA44', '#E7D2CF']
+const quotes = [{ phrase: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.", author: "Albert Einstein" },
+  { phrase: "The opposite of love is not hate, it's indifference. The opposite of art is not ugliness, it's indifference. The opposite of faith is not heresy, it's indifference. And the opposite of life is not death, it's indifference.", author: "Elie Wiesel" },
+  { phrase: "Have you ever noticed how ‘What the hell’ is always the right decision to make?", author: "Terry Johnson, Insignificance" },
+  { phrase: "How wonderful it is that nobody need wait a single moment before starting to improve the world.", author: "Anne Frank, Anne Frank's Tales from the Secret Annex: A Collection of Her Short Stories, Fables, and Lesser-Known Writings" },
+  { phrase: "Never doubt that a small group of thoughtful, committed, citizens can change the world. Indeed, it is the only thing that ever has", author: "Margaret Mead" },
+  { phrase: "We have to dare to be ourselves, however frightening or strange that self may prove to be.", author: "May Sarton" },
+  { phrase: "One, remember to look up at the stars and not down at your feet. Two, never give up work. Work gives you meaning and purpose and life is empty without it. Three, if you are lucky enough to find love, remember it is there and don't throw it away", author: "Stephen Hawking" }];
+
 const random = (max , min = 0) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -15,26 +23,40 @@ export function handle() {
 
 function Button(props) {
   return (
-    <button onClick={props.click} className="button" style={{ width: props.w, height: props.h, border:'1px solid #000', fontSize:'16px' }}>
+    <button onClick={props.click} className="button" style={{
+      color: props.back,
+      width: props.w,
+      height: props.h,
+      border: `1.5px solid ${props.back}`,
+      fontSize: '24px',
+      borderRadius:'15px'
+    }}>
       {props.children}
     </button>
   );
 }
 
-export function GroupButton() {
+export function GroupButton(props) {
+  const load = handle();
+  console.log(load)
+  const context = useContext(load);
+  console.log(context)
   return (
-    <div className='buttonsContainer' style={{display:'flex', justifyContent:'space-between'}}>
-      <div className='social'>
-        <Button w='50px' h='40px' >
-          <FaTumblr />
-        </Button>
-        <Button w='50px' h='40px'>
-          <FaTwitter />
+      <div className='buttonsContainer' style={{
+        display: 'flex',
+        justifyContent: 'space-between'
+      }}>
+        <div className='social' style={{display:'flex', gap:'25px'}}>
+          <Button w='70px' h='70px' back={props.back} >
+            <FaTumblr />
+          </Button>
+          <Button w='70px' h='70px' back={props.back}>
+            <FaTwitter />
+          </Button>
+        </div>
+        <Button w='230px' h='70px' click={load} back={props.back}>
+          Next Quote <FaCarrot />
         </Button>
       </div>
-      <Button w='150px' h='40px' click={handle}>
-        Next Quote <FaCarrot />
-      </Button>
-    </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,71 @@
 import React, { useState, useEffect, useContext } from 'react';
 import ReactDOM from 'react-dom/client';
-import { FaQuoteLeft } from "react-icons/fa";
+import { FaQuoteLeft, FaQuoteRight } from "react-icons/fa";
 import { GroupButton, handle } from './button';
 import './index.css';
 
-const load = handle()
+const load = handle();
+
+// export const MyContext = React.createContext(load);
 
 function App() {
   const [quote, setQuote] = useState(load.say);
+  // const context = useContext(MyContext);
   const [color, setColor] = useState(load.color);
+  useEffect(() => {
+    // setQuote(context.say)
+    // setColor(context.color)
+  }, color, quote)
 
   return (
-    <div class='container' style={{ width: '100vw', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', background:color }}>
-      <div class='card' style={{ width: '450px', height: '300px', boxShadow:'3px 3px 3px #000', padding:'10px', border:'1px solid #000' }}>
-        <p className='text'><span className='quotesStart'><FaQuoteLeft/>  </span>{quote.phrase}</p>
-        <p className='text'>{ quote.author }</p>
-        <GroupButton back={color} />
+    // <MyContext.Provider value={handle}>
+      <div className='container' style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        background: color,
+        // background: context.color,
+      }}>
+        <div className='card' style={{
+          maxWidth: '760px',
+          width: '100%',
+          // height:'100%',
+          background: '#fff',
+          padding: '35px',
+          border: `1px solid ${color}`,
+          borderRadius:'15px',
+          display: 'flex',
+          justifyContent:'space-between',
+          flexDirection: 'column'
+        }}>
+          <div className='quotes'>
+            <p className='text' style={{
+              padding: '0 0 0 25px',
+              color: color,
+              fontSize: '30px'
+            }}><FaQuoteLeft /> {quote.phrase}</p>
+            <div className='authorContainer' style={{
+              display: 'flex',
+              justifyContent:'flex-end',
+              width: '100%',
+            }}>
+              <p className='author' style={{
+                fontSize:'18px',
+                textTransform:'uppercase',
+                fontWeight:'500',
+                margin: '25px 0',
+                width: '350px',
+                textAlign:'right',
+                color: color
+              }}>- {quote.author}</p>
+            </div>
+          </div>
+          <GroupButton back={color} />
+        </div>
       </div>
-    </div>
+    // </MyContext.Provider>
   );
 }
 
